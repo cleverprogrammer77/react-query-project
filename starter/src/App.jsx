f9279b07bef93c8edf81b311a4386e24e6115d5c
priyanshu Jha <priyanshujha14@gmail.com>
2023-05-25 19:30:41 +0530

    first commit

---
+++ starter/src/App.jsx
@@ -0,0 +1,13 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import ToggleTheme from "./ToggleTheme";
+const App = () => {
+  return (
+    <main>
+      <ToggleTheme />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
